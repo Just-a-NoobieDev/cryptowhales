d0bd64b3e6815a5dba0acddec76efb10bb62fa93
Just-a-NoobieDev <carljames.roxas.m@gmail.com>
2022-08-11 17:17:46 +0800

    Add meta tags

--- src/components/footer/styles.jsx
+++ src/components/footer/styles.jsx
@@ -40,7 +40,7 @@ export const SubContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 50px;
-  margin: 0.1rem -;
+  margin: 0.1rem 0 1rem 0;
   flex-wrap: wrap;
   & > img {
     width: 100%;
